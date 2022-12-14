8de17d11fdb2ecccd526f04674e1f93b974df285
ZHANG YI <zhangyi000426@gmail.com>
2022-12-14 11:47:59 +0100

    update les requetes sur findAnime.js

--- routes/findAnime.js
+++ routes/findAnime.js
@@ -9,19 +9,23 @@ router.get("/:name/:director/:date", (req, res, next) => {
   var nameAnime = req.params.name;
   var director = req.params.director;
   var date = req.params.date;
-  var query = `SELECT DISTINCT ?item ?name ?director ?datePub WHERE {
-    ?item rdfs:label ?name;
-          p:P136 ?statement0;
-          p:P57  ?statement1;
-          p:P577 ?statement2.
-    ?statement0 (ps:P136/(wdt:P279*)) wd:Q1107.
-    ?statement1 ps:P57 ?directorLab.
-    ?directorLab rdfs:label ?director.
-    ?statement2 ps:P577 ?datePub.
-    FILTER(langMatches(lang(?name), "EN") && langMatches(lang(?director), "EN") && contains(lcase(?name), lcase('${nameAnime}')) && 
-    contains(lcase(?director), lcase('${director}')) && ?datePub > '${date}'^^xsd:dateTime).
-  }`;
-  
+  var query = `SELECT DISTINCT ?anime ?englishName ?japaneseTitle1 ?japaneseTitle2 ?duration (MIN(?r) AS ?releaseDate) ?directorName ?screenwriterName ?prodName
+  WHERE 
+  {
+    ?anime wdt:P31 wd:Q1107.
+    ?anime rdfs:label ?englishName.
+    FILTER(langMatches(lang(?englishName), "EN") && contains(lcase(?englishName), lcase('${nameAnime}')))
+    OPTIONAL{ ?anime wdt:P1476 ?japaneseTitle1. FILTER(langMatches(lang(?japaneseTitle1), "JA"))}
+    OPTIONAL{ ?anime wdt:P1705 ?japaneseTitle2. FILTER(langMatches(lang(?japaneseTitle2), "JA"))}
+    OPTIONAL{ ?anime wdt:P2047 ?duration.}  
+    OPTIONAL{ ?anime wdt:P577 ?r. FILTER(?r> '${date}'^^xsd:dateTime)}
+    OPTIONAL{ ?anime wdt:P57 ?director. ?director rdfs:label ?directorName.  FILTER( langMatches(lang(?directorName), "EN") && contains(lcase(?directorName), lcase('${director}')) ) }
+    OPTIONAL{ ?anime wdt:P58 ?screenwriter. ?screenwriter rdfs:label ?screenwriterName. FILTER(langMatches(lang(?screenwriterName), "EN"))}
+    OPTIONAL{ ?anime wdt:P272 ?prod. ?prod rdfs:label ?prodName.  FILTER(langMatches(lang(?prodName), "EN"))}
+  }
+  GROUP BY ?anime ?englishName ?japaneseTitle1 ?japaneseTitle2 ?duration ?releaseDate ?directorName ?screenwriterName ?prodName
+  ORDER BY ASC (?englishName)`;
+ 
   queryDispatcher.query(query).then((d) => {
     res.send(d.results.bindings);
     res.end();
