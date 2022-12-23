867d9a3d8bd5edc070c56eca97e04f35081e2f60
minhngo248 <minhngo248@users.noreply.github.com>
2022-12-23 18:11:19 +0700

    delete node_modules

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
