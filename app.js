14fece8dd97a443ba2b41548da6f48bf17a47c3e
minhngo248 <minhngo248@users.noreply.github.com>
2022-12-25 11:02:40 +0700

    fix bug 2 in resultAnime

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/animes', animesRouter);
 app.use('/api/anime', animeFindRouter);
-app.use('/api', resultAnime);
+app.use('/api/result', resultAnime);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
