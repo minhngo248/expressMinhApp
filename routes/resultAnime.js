36d5e818ca1f866f3700e7cf7498ffc663450380
minhngo248 <minhngo248@users.noreply.github.com>
2022-12-25 10:50:50 +0700

    fix bug resultAnime

--- routes/resultAnime.js
+++ routes/resultAnime.js
@@ -5,7 +5,7 @@ var SPARQLQueryDispatcher = require('./SPARQLQueryDispatcher');
 const endpointUrl = "https://query.wikidata.org/sparql";
 const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
 
-router.get("/", (req, res, next) => {
+router.get("/:idAnime", (req, res, next) => {
   var idAnime = req.params.idAnime;
   var query = `SELECT DISTINCT ?predicate ?target WHERE {
     wd:${idAnime} ?predicate ?target.
